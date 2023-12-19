542518c2345cea889d14425d946f5780086bcc53
frontsirouto <javaholic73@gmail.com>
2023-12-19 12:12:10 +0900

    ♻️ Main Component Add

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,5 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
-import { Vercel } from "@/components/Vercel";
-import { Links } from "@/components/Links";
-import { Headline } from "@/components/Headline";
+import { Main } from "@/components/Main";
 
 export default function Home() {
   return (
@@ -13,12 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} `}>
-        <Headline page="about" />
-        <Vercel />
-
-        <Links />
-      </main>
+      <Main page="about" />
     </>
   );
 }
